8c0f83e102b17ffe4ed2ba4e9cb99a866de3b3de
Sushant Hegde <sushanthegde@sharechat.co>
2021-10-10 20:24:05 +0530

    First Commit

---
+++ Shared/TestGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitApp.swift
+//  Shared
+//
+//  Created by Sushant Hegde on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TestGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
